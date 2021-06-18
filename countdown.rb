c60a489117ecebc044aa2a3166175931488aaa37
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-18 14:33:48 -0400

    Archiving Flatiron Code.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
-#write your code here
+def countdown(time)
+# loop function while time in seconds is greater than 0
+  while time > 0
+    # output a string of the amount of seconds left
+    puts "#{time} SECOND(S)!"
+    # decrement time
+    time -= 1
+  end
+  # return HAPPY NEW YEAR!
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(time)
+    # loop function while time in seconds is greater than 0
+      while time > 0
+        # output a string of the amount of seconds left
+        puts "#{time} SECOND(S)!"
+        # sleep for one second
+        sleep (1)
+        # decrement time
+        time -= 1
+    end
+    # return HAPPY NEW YEAR!
+    "HAPPY NEW YEAR!"
 end
